25c06a6206a9d49e5226bb3d6e0cb88302fb2bcd
Tai Ngo <taingo6798@gmail.com>
2024-06-07 21:23:08 +0700

    add daisyui

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,9 @@
 /** @type {import('tailwindcss').Config} */
+import daisy from 'daisyui'
 module.exports = {
   content: ['**/*.html', '**/*.tsx'],
   theme: {
     extend: {}
   },
-  plugins: []
+  plugins: [daisy]
 }
